21f6d636201bc2a4282a119f6d7fb78a3a47668b
Kyle <kylereaves@gmail.com>
2021-07-15 13:24:41 -0400

    committing on main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
 
-function App() {
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3lsZXJlYXZlcyIsImEiOiJja3FvaGcxb2MxbXBjMndvMXYwa2t6NnBlIn0.HhkYznr4TH2i6mnV1KIvRw';
+
+export default function App() {
+  const mapContainer = React.useRef<any>(null);
+  const [lng, setLng] = React.useState<number>(-74.0632);
+  const [lat, setLat] = React.useState<number>(40.7346);
+  const [zoom, setZoom] = React.useState<number>(12);
+
+  React.useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+    map.on('style.load', function () {
+      map.addSource('propertyData', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/kylemichaelreaves/landlord_data/master/jersey_city_HA.geojson'
+      });
+      map.addLayer({
+        id: 'property-layer',
+        source: 'propertyData',
+        type: 'circle',
+        paint: {
+          'circle-radius': 8,
+          'circle-opacity': .6,
+          'circle-color': 'red'
+        }
+      });
+
+      map.on('click', (e) => {
+        var features = map.queryRenderedFeatures(e.point, {
+          layers: ['property-layer']
+        });
+        if (!features.length) {
+          return;
+        };
+        var feature = features[0]
+        var popup = new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(e.lngLat)
+          .setHTML(
+            '<h3>' + feature.properties?.property_location + '</h3>' +
+            '<p>' + feature.properties?.owners_name + '</p>' +
+            '<p>' + feature.properties?.owners_mailing_address + '</p>' +
+            '<p>' + feature.properties?.city_state_zip + '</p>'
+          )
+          .addTo(map);
+      });
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">   
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
-}
-
-export default App;
+}      
---
+++ src/components/top-container/top-container.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+
+export default function TopContainer() {
+    
+    return (
+        <div className="top-container">
+            <div className='title'>
+                <h2>Find Your Landlord</h2>
+            </div>
+            <div className='get-help'>
+                <p>Organized Communities Stop Evictions</p>
+                <p>Get help! Get in touch!</p>
+                <p>About Find Your Landlord</p>
+            </div>
+        </div>
+    )
+}    
