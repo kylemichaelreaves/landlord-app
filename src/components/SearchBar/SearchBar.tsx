86efa84daada0d81cc4f9feb578c55ca5d08677a
Kyle <kylereaves@gmail.com>
2021-07-20 23:24:32 -0400

    made prop optional, console.log value for now…

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,18 +4,19 @@ import React from 'react';
 // onClick essentially searches the selected property and returns the result
 
 interface Props {
-    address: string;
+    address?: string;
 }
 
 export default function SearchBar(address: Props) {
     const [searchTerm, setSearchTerm] = React.useState<string>("");
+    
 
     return(
         <div className='search-bar'>
             <input
                 type='text'
                 value={searchTerm}
-                onChange={(e: any) => setSearchTerm(e.target.value)}
+                onChange={(e: any) => console.log(e.target.value)}
                 placeholder='Search for property…'
                 />
         </div>
