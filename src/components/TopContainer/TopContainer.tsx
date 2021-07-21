762992572721a7a4a00faf31224749a6142190bd
Kyle <kylereaves@gmail.com>
2021-07-20 23:27:53 -0400

    deleted the comment

--- src/components/TopContainer/TopContainer.tsx
+++ src/components/TopContainer/TopContainer.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import '../../App.css';
 
+
+
 export default function TopContainer() {
     var propertyAddress = document.getElementById('propertyAddress');
     var ownerName = document.getElementById('ownerName');
     var ownerAddress = document.getElementById('ownerAddress');
     var associatedProperties = document.getElementById('associatedProperties');
+
     
+
     return (
+        
         <div className="top-container">
-        <h3>FindYourLandlord by North NJ DSA</h3>
-        {/* spans id must match their instatiation as vars */}
-        <div><strong>Address:</strong> <span id='propertyAddress'></span></div>
-        <div><strong>Owner:</strong> <span id='ownerName'></span></div>
-        <div><strong>Owner's Address:</strong> <span id='ownerAddress'></span></div>
-        <div><strong>Number of Associated Properties with Owner: </strong><span id='associatedProperties'></span></div>
-      </div>
+            <div className='title'>FindYourLandlord by North NJ DSA</div>
+            
+            <div><strong>Address:</strong> <span id='propertyAddress'></span></div>
+            <div><strong>Owner:</strong> <span id='ownerName'></span></div>
+            <div><strong>Owner's Address:</strong> <span id='ownerAddress'></span></div>
+            <div><strong>Number of Associated Properties with Owner: </strong><span id='associatedProperties'></span></div>
+        </div>
+      
+        
+
+
 
     )
-}    
+}
