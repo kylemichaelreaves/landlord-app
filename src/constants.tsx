db5f061d4de4d2a02c5358ed330312d98979f4bb
Kyle <kylereaves@gmail.com>
2021-08-08 02:06:54 -0400

    additions

--- src/constants.tsx
+++ src/constants.tsx
@@ -11,3 +11,16 @@ export var color1 = "#1e1231";
 export var black = "#000";
 export var white = "#fff";
 export var gray = "#808080";
+
+export var defaultColors = [
+    "case",
+      [">=", ["to-number", ["get", 'num_asc_properties']], 100],
+      color4,
+      [">=", ["to-number", ["get", 'num_asc_properties']], 10],
+      color3,
+      [">=", ["to-number", ["get", 'num_asc_properties']], 3],
+      color2,
+      [">", ["to-number", ["get", 'num_asc_properties']], 0],
+      color1,
+      white
+  ];
