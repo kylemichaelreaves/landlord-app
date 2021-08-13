4cfadafbb858ecaed2a448b26afb7d0f78ef417d
Kyle <kylereaves@gmail.com>
2021-08-13 17:01:52 -0400

    setting up tests…

---
+++ src/components/ResultsTable/__tests__/ResultsTable.tests.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { screen, render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import ResultsTable from '../../ResultsTable';
+
---
+++ src/components/TopContainer/__tests__/TopContainer.tests.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { screen, render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import TopContainer from '../../TopContainer';
