b60e53d7c9ed0460d61648fe13f4ca8b9c8ad7cd
Kyle <kylereaves@gmail.com>
2021-08-16 18:36:25 -0400

    three tests added

--- src/components/TopContainer/__tests__/TopContainer.tests.jsx
+++ src/components/TopContainer/__tests__/TopContainer.tests.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { screen, render, waitFor } from '@testing-library/react';
+import { screen, render, waitFor, getByText } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import TopContainer from '../TopContainer';
 
-test('something is supposed to happen', () => {
+
+test('title shows up in the document', () => {
+    render(<TopContainer />);
+    expect(screen.getByText('FindYourLandlord')).toBeInTheDocument();
+})
+
+test('ResultsTable does not appear', () => {
+    render(<TopContainer />);
+    const resultsTable = screen.getByRole('table')
+    expect(resultsTable).not.toBeInTheDocument();
+
+})
+    
+    
+    
+
+test('search bar appears in top container', () => {
+    render(<TopContainer />);
+    const searchBar = screen.getByPlaceholderText('Search for property…')
+    expect(searchBar).toBeInTheDocument();
 
 })
