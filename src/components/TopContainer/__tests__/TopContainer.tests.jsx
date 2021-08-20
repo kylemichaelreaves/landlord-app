dea3c22d859fc8760ad89b6f544562f9f937e88e
Kyle <kylereaves@gmail.com>
2021-08-19 20:44:11 -0400

    tests refined

--- src/components/TopContainer/__tests__/TopContainer.tests.jsx
+++ src/components/TopContainer/__tests__/TopContainer.tests.jsx
@@ -3,6 +3,9 @@ import { screen, render, waitFor, getByText } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import TopContainer from '../TopContainer';
+import ResultsTable from "/Users/kylereaves/Desktop/landlord-app/src/components/ResultsTable/ResultsTable";
+
+import renderer from 'react-test-renderer';
 
 
 test('title shows up in the document', () => {
@@ -10,19 +13,13 @@ test('title shows up in the document', () => {
     expect(screen.getByText('FindYourLandlord')).toBeInTheDocument();
 })
 
-test('ResultsTable does not appear', () => {
-    render(<TopContainer />);
-    const resultsTable = screen.getByRole('table')
-    expect(resultsTable).not.toBeInTheDocument();
-
+test("ResultsTable shouldn't appear if there aren't props passed to it", () => {
+    const component = renderer.create(<ResultsTable />)
+    console.log("component", component)
+    expect(component).toBeNull();
 })
-    
-    
-    
 
-test('search bar appears in top container', () => {
-    render(<TopContainer />);
-    const searchBar = screen.getByPlaceholderText('Search for property…')
-    expect(searchBar).toBeInTheDocument();
-
+test('component should not be in document if there are no props passed to it', () => {
+    const component = renderer.create(<ResultsTable />)
+    expect(component).toBeInTheDocument();
 })
