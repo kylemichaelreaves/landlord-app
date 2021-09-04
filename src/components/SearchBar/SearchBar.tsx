c7a57d450656f85164940d378fe3505e31d7fc7b
Kyle <kylereaves@gmail.com>
2021-09-04 19:56:36 -0400

    added async fetch function

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -16,6 +16,7 @@ export default function SearchBar({ onSearch }: Props) {
     function renderClearButton() {
         if (searchTerm) {
             return <button
+                
                 onClick={(e: React.FormEvent<HTMLButtonElement>) => setSearchTerm(blankSearch)}>
                 <AiOutlineCloseSquare size={25} />
             </button>
@@ -26,6 +27,7 @@ export default function SearchBar({ onSearch }: Props) {
     function renderSearchButton() {
         if (searchTerm !== "") {
             return <button
+                
                 onClick={(e: React.FormEvent<HTMLButtonElement>) => setSearchTerm(searchTerm)}>
                 <AiOutlineSearch size={25} />
             </button>
@@ -33,6 +35,17 @@ export default function SearchBar({ onSearch }: Props) {
             return <button></button>
         }
     }
+
+    async function fetchData (filterValue: string) {
+        let url = "https://raw.githubusercontent.com/kylemichaelreaves/landlord_data/master/test_data.geojson";
+        const data = await fetch(url).then((r) => r.json());
+        console.log(data)
+        
+        return data.features.filter((obj: GeoJSON.Feature) => obj.properties?.propertyLocation.includes(filterValue))
+        .map((obj: GeoJSON.GeoJsonProperties) => obj?.properties)
+    }
+
+    
 
     React.useEffect(() => {
         if (searchTerm) {
